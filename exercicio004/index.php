d36ace7497c201fed3edcf05d750b59b20799e12
Roselene Aparecida da Silva Yokoyama <roselene_31@hotmail.com>
2025-05-14 22:14:59 -0300

    exercicios001

---
+++ exercicio004/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+    <form name="formulario" method="post" action="verificar.php">
+        Idade: <input  type="text" name="idade"><br>
+        <button>type="submit">Verificar Idade</button>
+    </form>
+    
+</body>
+</html>
